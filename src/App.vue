afe59b5744644c15c2bfcd476ff5948d7f74adf6
Felipe Alvarez <felipeag06@gmail.com>
2017-04-06 22:32:52 -0500

    Home design basics added

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,25 @@ export default {
   padding: 0 !important;
   overflow: hidden;
 }
+
+@font-face {
+    font-family: 'jaap-regular';
+    src: url('./assets/fonts/regular/jaapokki-regular.eot'),
+         url('./assets/fonts/regular/jaapokki-regular.woff') format('woff'),
+         url('./assets/fonts/regular/jaapokki-regular.ttf') format('truetype'),
+         url('./assets/fonts/regular/jaapokki-regular.svg') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'jaap-substract';
+    src: url('./assets/fonts/substract/jaapokki-substract.eot'),
+         url('./assets/fonts/substract/jaapokki-substract.woff') format('woff'),
+         url('./assets/fonts/substract/jaapokki-substract.ttf') format('truetype'),
+         url('./assets/fonts/substract/jaapokki-substract.svg') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,54 @@
 <template lang="html">
-  <div class="home-elements fill-height bg-transition">
-    <div class="row">
-      <div class="col-sm-12">
-        <h1 class="text-center home-text t-name">Felipe Alvarez</h1>
+  <div class="fill-height home-elements-container bg-pattern bg-transition">
+    <div class="page-title-container">
+      <div class="page-title">
+        <h1 class="text-center home-text t-name">F E L I P E . A L V A R E Z</h1>
         <h3 class="text-center home-text t-description">UX designer and Frontend developer</h3>
       </div>
-
-
     </div>
 
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+  export default {
+    name: 'Home'
+  }
 </script>
 
 <style lang="css">
-.home-text {
-  color:#FFF;
-}
-.home-elements {
-  background: white;
-}
-.bg-transition{
-  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#01359f+0,7f24b7+88,7f24b7+100 */
-  background: #01359f; /* Old browsers */
-  background: -moz-linear-gradient(45deg,  #01359f 0%, #7f24b7 88%, #7f24b7 100%); /* FF3.6-15 */
-  background: -webkit-linear-gradient(45deg,  #01359f 0%,#7f24b7 88%,#7f24b7 100%); /* Chrome10-25,Safari5.1-6 */
-  background: linear-gradient(45deg,  #01359f 0%,#7f24b7 88%,#7f24b7 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#01359f', endColorstr='#7f24b7',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-}
+  .home-text {
+    color:#FFF;
+  }
+
+  .t-name {
+    font-size: 4em;
+    font-family: 'jaap-substract';
+  }
+
+  .t-description {
+    font-family: 'jaap-regular';
+  }
+
+  .home-elements-container{
+    height: 100%;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    -webkit-box-align: center;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .bg-transition {
+    /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#01359f+0,7f24b7+88,7f24b7+100 */
+    background: #01359f; /* Old browsers */
+    background: -moz-linear-gradient(45deg,  #01359f 0%, #7f24b7 88%, #7f24b7 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(45deg,  #01359f 0%,#7f24b7 88%,#7f24b7 100%); /* Chrome10-25,Safari5.1-6 */
+    /*TODO: Pattern must be moved else where since it will be animated*/
+    background: url('../assets/img/home/pattern.png') no-repeat, linear-gradient(45deg,  #01359f 0%,#7f24b7 88%,#7f24b7 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#01359f', endColorstr='#7f24b7',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+  }
 </style>
